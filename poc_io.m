427caca3880ad7de9ad7ee4d99cd2d5972b46737
Eduardo Costa <m4c0@users.noreply.github.com>
2025-10-31 18:08:37 +0000

    Being able to find a device

--- poc_io.m
+++ poc_io.m
@@ -1,18 +1,26 @@
 @import Foundation;
 @import IOKit.hid;
 
-static void fn(const void * ptr, void * n) {
-  NSLog(@"%@", ptr);
-}
-
 int main() {
-  NSDictionary * match = @{};
+  // VendorID=32905 ProductID=12
+  NSDictionary * match = @{
+    @kIOHIDVendorIDKey: @32905,
+    @kIOHIDPrimaryUsagePageKey: @0xFF00, // Vendor-specific
+  };
 
   IOHIDManagerRef mgr = IOHIDManagerCreate(kCFAllocatorDefault, 0);
   IOHIDManagerOpen(mgr, 0);
   IOHIDManagerSetDeviceMatching(mgr, (CFDictionaryRef) match);
 
-  CFSetRef set = IOHIDManagerCopyDevices(mgr);
-  CFSetApplyFunction(set, fn, nil);
+  IOHIDDeviceRef dev = (__bridge IOHIDDeviceRef) [(__bridge NSSet *)IOHIDManagerCopyDevices(mgr) anyObject];
+  if (!dev) {
+    NSLog(@"Could not find a suitable device");
+    return 1;
+  }
+  NSLog(@"%@", dev);
+
+  unsigned char d1[64] = { 0x03 };
+  IOHIDDeviceSetReport(dev, kIOHIDReportTypeOutput, 0, d1, sizeof(d1));
+  NSLog(@"done");
 }
 
